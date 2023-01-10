42cf74cc32548f30df23bef0da36486c5d4f4844
filipj169 <jamrichfilip16@gmail.com>
2023-01-10 18:08:01 +0100

    starter

---
+++ src/components/HeaderBackground.vue
@@ -0,0 +1,22 @@
+<template>
+    <header class="main-header">
+        <div class="flex-container container">
+            <h3 class="main-header__heading">devjobs</h3>
+            <div class="theme-switch">
+                <img src="@/assets/images/desktop/icon-sun.svg" alt="light theme">
+                <button type="button" class="btn-switch"></button>
+                <img src="@/assets/images/desktop/icon-moon.svg" alt="dark theme">
+            </div>
+        </div>
+    </header>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss">
+@import "@/assets/scss/main.scss";
+</style>
--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,34 @@
 import { createStore } from 'vuex'
+import axios from "axios"
 
-export default createStore({
+const store = createStore({
   state: {
+    jobs: [],
   },
   getters: {
   },
   mutations: {
+    SET_JOBS: (state, jobs) => {
+      state.jobs = jobs
+    }
   },
   actions: {
   },
   modules: {
   }
 })
+
+export default store
+
+/*
+const setJobsToStore = () => {
+  axios
+  .get("/public/data.json")
+  .then(response => {
+    store.commit("SET_JOBS", response.data)
+  })
+}
+
+setJobsToStore()
+
+*/
