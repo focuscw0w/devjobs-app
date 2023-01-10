5b977e3259833045ba273c142681f17b52c1b81f
filipj169 <jamrichfilip16@gmail.com>
2023-01-10 19:07:37 +0100

    switch button

--- src/components/HeaderBackground.vue
+++ src/components/HeaderBackground.vue
@@ -4,7 +4,7 @@
             <h3 class="main-header__heading">devjobs</h3>
             <div class="theme-switch">
                 <img src="@/assets/images/desktop/icon-sun.svg" alt="light theme">
-                <button type="button" class="btn-switch"></button>
+                <SwitchButton />
                 <img src="@/assets/images/desktop/icon-moon.svg" alt="dark theme">
             </div>
         </div>
@@ -12,8 +12,9 @@
 </template>
 
 <script>
+import SwitchButton from "@/components/SwitchButton.vue";
 export default {
-
+    components: { SwitchButton }
 }
 </script>
 
---
+++ src/components/SwitchButton.vue
@@ -0,0 +1,32 @@
+<template>
+  <label for="checkbox" class="switch" @click="toggleTheme">
+    <input type="checkbox" :checked="darkTheme" />
+    <span class="slider"></span>
+  </label>
+</template>
+
+<script>
+import { reactive, toRefs } from "@vue/reactivity";
+export default {
+  setup() {
+    // refactor with vuex
+
+    const state = reactive({
+      darkTheme: false,
+    });
+
+    const toggleTheme = () => {
+      state.darkTheme = !state.darkTheme;
+    };
+
+    return {
+      ...toRefs(state),
+      toggleTheme,
+    };
+  },
+};
+</script>
+
+<style lang="scss">
+@import "@/assets/scss/main.scss";
+</style>
