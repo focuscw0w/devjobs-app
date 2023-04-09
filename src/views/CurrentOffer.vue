431cc8bd8bfe464d26c18e4478f460754f7150ba
filipj169 <jamrichfilip16@gmail.com>
2023-04-09 19:05:00 +0200

    some styling

--- src/views/CurrentOffer.vue
+++ src/views/CurrentOffer.vue
@@ -15,69 +15,73 @@
     <article class="offer__details">
       <div class="flex-container">
         <span class="offer__details-time">1w ago</span>
-        •
+        <span class="offer__details-splitter">•</span>
         <span class="offer__details-type">Part Time</span>
       </div>
-      <div class="flex-container">
+      <div class="offer__details-job">
         <h3 class="offer__details-heading">Senior Software Engineer</h3>
         <button type="submit" class="offer__company-apply">Apply Now</button>
       </div>
       <p class="offer__details-location">United Kingdom</p>
-      <p class="offer__details-paragraph">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-        mollit anim id est laborum.
-      </p>
-      <h5 class="offer__details-sub-heading">Requirements</h5>
-      <p class="offer__details-paragraph">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam quo
-        deleniti nesciunt nulla sed. Maxime, quos beatae? Quisquam dolorum,
-        facere recusandae impedit odit animi tempore accusamus, dignissimos,
-        similique illum repellat!
-      </p>
-      <ul class="offer__details-requirements">
-        <li class="offer__details-requirements__item">
-          Morbi interdum mollis sapien. Sed
-        </li>
-        <li class="offer__details-requirements__item">
-          Phasellus lacinia magna a ullamcorper lacreet, lectus arcu pulvinar
-          risus
-        </li>
-        <li class="offer__details-requirements__item">
-          Mauris nibh felis, adipisicing varius, adipiscing in, lacinia vel,
-          tellus. Suspendisse ac urna. Etiam pellentesque mauris ut lectus.
-        </li>
-        <li class="offer__details-requirements__item">
-          Morbi interdum mollis sapien. Sed
-        </li>
-      </ul>
-      <h5 class="offer__details-sub-heading">What You Will Do</h5>
-      <p class="offer__details-paragraph">
-        Quo quaerat expedita eos recusandae ullam aut voluptas autem vel
-        expedita ducimus in reprehenderit molestiae. Ut quia harum eos nulla
-        molestiae aut reiciendis officiis est voluptatum deleniti qui provident
-        saepe. Ut porro natus rem aliquid excepturi et quae nostrum.
-      </p>
-      <ul class="offer__details-requirements-num">
-        <li class="offer__details-requirements__item">
-          Morbi interdum mollis sapien. Sed
-        </li>
-        <li class="offer__details-requirements__item">
-          Phasellus lacinia magna a ullamcorper lacreet, lectus arcu pulvinar
-          risus
-        </li>
-        <li class="offer__details-requirements__item">
-          Mauris nibh felis, adipisicing varius, adipiscing in, lacinia vel,
-          tellus. Suspendisse ac urna. Etiam pellentesque mauris ut lectus.
-        </li>
-        <li class="offer__details-requirements__item">
-          Morbi interdum mollis sapien. Sed
-        </li>
-      </ul>
+
+      <div class="offer__details-info">
+        <p class="offer__details-paragraph">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+        <h5 class="offer__details-sub-heading">Requirements</h5>
+
+        <p class="offer__details-paragraph">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam quo
+          deleniti nesciunt nulla sed. Maxime, quos beatae? Quisquam dolorum,
+          facere recusandae impedit odit animi tempore accusamus, dignissimos,
+          similique illum repellat!
+        </p>
+        <ul class="offer__details-requirements">
+          <li class="offer__details-requirements__item">
+            Morbi interdum mollis sapien. Sed
+          </li>
+          <li class="offer__details-requirements__item">
+            Phasellus lacinia magna a ullamcorper lacreet, lectus arcu pulvinar
+            risus
+          </li>
+          <li class="offer__details-requirements__item">
+            Mauris nibh felis, adipisicing varius, adipiscing in, lacinia vel,
+            tellus. Suspendisse ac urna. Etiam pellentesque mauris ut lectus.
+          </li>
+          <li class="offer__details-requirements__item">
+            Morbi interdum mollis sapien. Sed
+          </li>
+        </ul>
+        <h5 class="offer__details-sub-heading">What You Will Do</h5>
+        <p class="offer__details-paragraph">
+          Quo quaerat expedita eos recusandae ullam aut voluptas autem vel
+          expedita ducimus in reprehenderit molestiae. Ut quia harum eos nulla
+          molestiae aut reiciendis officiis est voluptatum deleniti qui
+          provident saepe. Ut porro natus rem aliquid excepturi et quae nostrum.
+        </p>
+        <ul class="offer__details-requirements-num">
+          <li class="offer__details-requirements__item">
+            Morbi interdum mollis sapien. Sed
+          </li>
+          <li class="offer__details-requirements__item">
+            Phasellus lacinia magna a ullamcorper lacreet, lectus arcu pulvinar
+            risus
+          </li>
+          <li class="offer__details-requirements__item">
+            Mauris nibh felis, adipisicing varius, adipiscing in, lacinia vel,
+            tellus. Suspendisse ac urna. Etiam pellentesque mauris ut lectus.
+          </li>
+          <li class="offer__details-requirements__item">
+            Morbi interdum mollis sapien. Sed
+          </li>
+        </ul>
+      </div>
     </article>
   </section>
 </template>
